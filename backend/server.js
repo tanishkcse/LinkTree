0b703d683ea97c37c76d2180bff5a5648070f2a3
tanishkcse <112652811+tanishkcse@users.noreply.github.com>
2025-03-06 01:39:56 +0530

    configure the api calls to connect with backend on deployment

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,13 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+//app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(morgan());
